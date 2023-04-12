9c422a783931f4c0c7cb51be68594d7c054a345f
GimmeSpoon <skcjfdnd1996@gmail.com>
2023-04-12 23:50:28 +0900

    fix: ddp error

--- eniat/pytorch/trainer.py
+++ eniat/pytorch/trainer.py
@@ -126,7 +126,7 @@ class TorchTrainer(Trainer):
                 if self._dist:
                     if self.conf.distributed.type == "DDP":
                         if current_process().name == "MainProcess":
-                            spawn(self.dist, (fn.__name__, *args),nprocs=self.conf.distributed.local_size, join=True)
+                            spawn(self.dist, (fn.__name__),nprocs=self.conf.distributed.local_size, join=True)
                     return self.dist(os.environ['LOCAL_RANK'], fn.__name__, *args)
                 else:
                     return fn(*args)
