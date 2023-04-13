f201d4cd2e90a40388fdb0d23bc8c159f6963462
GimmeSpoon <skcjfdnd1996@gmail.com>
2023-04-13 16:56:17 +0900

    fix: fixed reference

--- eniat/pytorch/trainer.py
+++ eniat/pytorch/trainer.py
@@ -189,7 +189,7 @@ class TorchTrainer(Trainer):
             self.log.warning("Scheduler is not defined. Edit the configuration if this is not what you wanted.")
         
         if self._dist:
-            model = DDP(self.learner.model).compile() if self.compile else DDP(self.learner.model)
+            model = DDP(model).compile() if self.compile else DDP(model)
             optim = self.get_dist_opt(self.conf.distributed.optimizer, self.learner.model.get_params())
         
         # instantiate learner
