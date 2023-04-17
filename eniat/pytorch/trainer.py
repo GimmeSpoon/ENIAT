a29761a42bcfce056d38f29618cedf576d691976
GimmeSpoon <skcjfdnd1996@gmail.com>
2023-04-17 10:08:15 +0900

    fix: fixed tqdm redirect error

--- eniat/pytorch/trainer.py
+++ eniat/pytorch/trainer.py
@@ -119,7 +119,7 @@ class TorchTrainer(Trainer):
     def fit(self, device:int, silent:bool=False):
         current_step = 0
         with _stdout() as stdout:
-            for epoch in (epoch_bar:=tqdm(range(self.init_step, self.max_step if self.unit == 'epoch' else 1), desc='Training', unit='epoch', position=0, leave=False, disable=True if self.unit != 'epoch' else silent, file=stdout)):
+            for epoch in (epoch_bar:=tqdm(range(self.init_step, self.max_step if self.unit == 'epoch' else 1), desc='Training', unit='epoch', position=0, leave=False, disable=True if self.unit != 'epoch' else silent, file=stdout, dynamic_ncols=True)):
                 for batch in (step_bar:=tqdm(self.loader, desc='Batch', unit='step', position=1, leave=False, disable=silent, _file=_stdout)):
                     batch = self.to_tensor(batch)
                     tr_loss = self.learner.fit(batch, device, self.log)
@@ -326,7 +326,7 @@ class TorchDistributedTrainer(TorchTrainer):
         self.prepare(device, 'fit')
         current_step = 0
         with _stdout() as stdout:
-            for epoch in (epoch_bar:=tqdm(range(self.init_step, self.max_step if self.unit == 'epoch' else 1), desc='Training', unit='epoch', position=0, leave=False, disable=True if self.unit != 'epoch' else silent, file=stdout)):
+            for epoch in (epoch_bar:=tqdm(range(self.init_step, self.max_step if self.unit == 'epoch' else 1), desc='Training', unit='epoch', position=0, leave=False, disable=True if self.unit != 'epoch' else silent, file=stdout, dynamic_ncols=True)):
                 for batch in (step_bar:=tqdm(self.loader, desc='Batch', unit='step', position=1, leave=False, disable=silent, file=_stdout)):
                     batch = self.to_tensor(batch)
                     tr_loss = self.learner.fit(batch, device, self.log)
