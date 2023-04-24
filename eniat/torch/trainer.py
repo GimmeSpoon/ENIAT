4b4671d1f34c71b1bb9bdf49d9f17bb251b097fe
GimmeSpoon <skcjfdnd1996@gmail.com>
2023-04-24 18:04:58 +0900

    fix: checkpoint path acquired from hydraconfig

--- eniat/torch/trainer.py
+++ eniat/torch/trainer.py
@@ -93,17 +93,19 @@ class TorchTrainer(Trainer):
         self.max_step = resumed['maxstep']
 
     def _save_model(self, timestep:int=None) -> None:
-        Path(os.path.join(self.conf.working_dir, 'checkoints')).mkdir(parents=True, exist_ok=True)
+        hc = HydraConfig.get()
+        Path(os.path.join(hc.runtime.output_dir, 'checkoints')).mkdir(parents=True, exist_ok=True)
         torch.save(self.learner.model.state_dict(), os.path.join( self.conf.working_dir , f'checkpoints/model_{timestep}.cpt' if timestep else 'checkpoints/model.cpt'))
 
     def _save_train_state(self, timestep:int) -> None:
+        hc = HydraConfig.get()
         train_state = self.learner.get_optimizer().state_dict()['optimizer']
         train_state['unit'] = self.unit
         train_state['rng_state'] = self.get_rand_state()
         train_state['timestep'] = timestep
         train_state['maxstep'] = self.max_step
         train_state['distributed'] = self._dist
-        Path(os.path.join(self.conf.working_dir, 'checkoints')).mkdir(parents=True, exist_ok=True)
+        Path(os.path.join(hc.runtime.output_dir, 'checkoints')).mkdir(parents=True, exist_ok=True)
         torch.save(train_state, os.path.join(self.conf.working_dir, f'checkpoints/state_{timestep}.cpt'))
 
     def _save_checkpoint(self, timestep:int, unit:Literal['epoch', 'step'], force:bool=False) -> None:
