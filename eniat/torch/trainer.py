d01e366a97036084836a4f7fce5f762c1453b1d3
GimmeSpoon <skcjfdnd1996@gmail.com>
2023-04-24 17:18:48 +0900

    fix: torchtrainer not loaded in eniat

--- eniat/torch/trainer.py
+++ eniat/torch/trainer.py
@@ -77,9 +77,6 @@ def torchload(cfg:DictConfig, log:L):
 
         # instantiate trainer
         trainer = getattr(import_module('.pytorch', 'eniat'), 'TorchTrainer')(course=_courses, learner=learner, conf=cfg.trainer, logger=log)
-    else:
-        trainer = getattr(import_module('.pytorch', 'eniat'), 'TorchDistributedTrainer')(conf=cfg.trainer, data_conf=cfg.data, learner_conf=cfg.learner, logger_conf=cfg.logger)
-        log.info("Distributed Learning (Torch) is configured.")
     
     if trainer:
         log.info("Trainer instance created.")
